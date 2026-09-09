22c41938374f2fb591264428d61610985f196022
邢斐 <xing66fei@126.com>
2026-09-09 15:48:06 +0800

    fix: load env for prisma commands

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,7 @@
+import nextEnv from "@next/env";
 import { defineConfig, env } from "prisma/config";
+
+nextEnv.loadEnvConfig(process.cwd());
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
